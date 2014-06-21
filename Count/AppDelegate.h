4ec41009712be5391bc81e82e57bec2f185714c8
Nanami Tomozoe <nanami@tomozoe.com>
2014-06-21 17:50:11 +0900

    始めてのギット

---
+++ Count/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Count
+//
+//  Created by nanami on 2013/10/26.
+//  Copyright (c) 2013年 nanami. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Count/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Count
+//
+//  Created by nanami on 2013/10/26.
+//  Copyright (c) 2013年 nanami. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
